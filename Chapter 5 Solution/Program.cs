a6856a7907848251d8e24e9eaeb99e6778be7beb
Damilare John <damilare.johnms@outlook.com>
2021-06-29 05:20:48 -0700

    Add project files.

---
+++ Chapter 5 Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter_5_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
